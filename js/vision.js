1619799a2397d58c436c8f024a151ca81bc8d737
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-15 16:18:19 +0100

    introduce a vision view

--- js/vision.js
+++ js/vision.js
@@ -1,6 +1,15 @@
 (function(){
-    var canvas = document.getElementById('vision');
-    var context = canvas.getContext('2d');
+    var VisionView = function(canvas){
+	this.context = canvas.getContext('2d');
+	this.width = canvas.width;
+	this.height = canvas.height;
+	this.update();
+    }
+    VisionView.prototype.update = function(){
+	this.context.fillRect(0, 0, this.width, this.height);
+    }
 
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    var canvas = document.getElementById('vision');
+
+    new VisionView(canvas);
 })();
