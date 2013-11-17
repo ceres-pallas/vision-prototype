65c5a8ca5568408e6e0d94c631e3587fa4f65f7c
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-17 14:26:26 +0100

    abstract a Radar model and view

--- js/vision.js
+++ js/vision.js
@@ -72,27 +72,49 @@
 
     }
 
+    var Radar = function(heading, halfAngle){
+	Listener.call(this);
+	this.heading = heading;
+	this.halfAngle = halfAngle;
+    }
+    Radar.prototype = new Listener();
+
+    var RadarView = function(canvas, model){
+	this.context = canvas.getContext('2d');
+	this.width = canvas.width;
+	this.height = canvas.height;
+	this.model = model;
+	this.model.addListener(this.update.bind(this));
+	this.update();
+    }
+    RadarView.prototype.update = function(){
+	var context = this.context;
+	context.fillStyle = 'black';
+	context.fillRect(0, 0, this.width, this.height);
+
+	var heading = this.model.heading;
+	var halfAngle = this.model.halfAngle;
+	var radius = Math.min(topCanvas.width/2, topCanvas.height/2) - 10;
+	var x0 = this.width/2;
+	var y0 = this.height/2;
+	var x1 = radius * Math.cos(heading + -halfAngle) + x0;
+	var y1 = radius * Math.sin(heading + -halfAngle) + y0;
+	context.strokeStyle = 'red';
+	context.beginPath();
+	context.moveTo(x0, y0);
+	context.lineTo(x1, y1);
+	context.arc(x0, y0, radius, heading - halfAngle, heading + halfAngle);
+	context.closePath();
+	context.stroke();
+
+    }
+
     var visionCanvas = document.getElementById('vision');
     var topCanvas = document.getElementById('top');
 
-    var context = topCanvas.getContext('2d');
-    context.fillRect(0, 0, topCanvas.width, topCanvas.height);
-
-    var heading = -Math.PI/6;
-    var halfAngle = Math.PI/3;
-    var radius = Math.min(topCanvas.width/2, topCanvas.height/2) - 10;
-    var x0 = topCanvas.width/2;
-    var y0 = topCanvas.height/2;
-    var x1 = radius * Math.cos(heading + -halfAngle) + x0;
-    var y1 = radius * Math.sin(heading + -halfAngle) + y0;
-    context.strokeStyle = 'red';
-    context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    context.arc(x0, y0, radius, heading - halfAngle, heading + halfAngle);
-    context.closePath();
-    context.stroke();
-
     var vision = new Vision();
     new VisionView(visionCanvas, vision);
+
+    var radar = new Radar(-Math.PI/6, Math.PI/3);
+    new RadarView(topCanvas, radar);
 })();
